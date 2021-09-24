9346466517d763ffee4a38b75f2f95903e447f20
Tragedt <54042397+Tragedy-dev@users.noreply.github.com>
2021-09-24 09:27:56 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Wed Sep 25 2019                                           */
+/*    Description:  Mecanum Drivechain                                        */
+/*                                                                            */
+/*    Name: The Great Ethan Manninen                                          */
+/*    Date                                                                    */
+/*    Class:                                                                  */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    1, 2, 3, 4      
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+//public void vex::controller::rumble(const char . . . - - - . . . );
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+}
